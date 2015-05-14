0062921eee0f7dfe598e84c8a56b94b87ce6ec90
Jared <jaredtaylor@cbrc.net>
2015-05-14 09:17:58 -0400

    Initial Commit

---
+++ Inheritance/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Inheritance
+//
+//  Created by Jared Taylor on 5/14/15.
+//  Copyright (c) 2015 Jared Taylor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
